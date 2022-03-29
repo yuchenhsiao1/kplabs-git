e8a12e8f37e109140cf668d51dc1c055de47b16d
yuchenhsiao1 <97976927+yuchenhsiao1@users.noreply.github.com>
2022-03-29 11:27:01 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAVMU3AVIMIFDBHUX6"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
